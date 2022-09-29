7ed34947418e839cf9c887d9da7d02651f1dbd3b
uijeong <dmlwjd0202@naver.com>
2022-09-30 02:40:21 +0900

    commit

--- src/main/java/kr/ac/kopo/date/DayVO.java
+++ src/main/java/kr/ac/kopo/date/DayVO.java
@@ -3,20 +3,13 @@ package kr.ac.kopo.date;
 public class DayVO {
 
 	private String day;
-	private int count;
+	//private int count;
 	private int sum;
-	
 	public String getDay() {
 		return day;
 	}
 	public void setDay(String day) {
 		this.day = day;
-	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
 	}
 	public int getSum() {
 		return sum;
@@ -26,8 +19,9 @@ public class DayVO {
 	}
 	@Override
 	public String toString() {
-		return "DayVO [day=" + day + ", count=" + count + ", sum=" + sum + "]";
+		return "DayVO [day=" + day + ", sum=" + sum + "]";
 	}
+	
 	
 	
 	
